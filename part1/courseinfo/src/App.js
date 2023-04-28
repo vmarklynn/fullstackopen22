11c354d46586fc194f301189eef1d785502f8617
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2023-04-28 16:20:00 -0700

    Working course refactor in App.js

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -40,20 +40,18 @@ const Part = (props) => {
 };
 
 const App = () => {
-  const course = {
-    name: "Half stack application development",
-    parts: [
-      { name: "Fundamentals of React", exercises: 10 },
-      { name: "Using props to pass data", exercises: 7 },
-      { name: "State of a component", exercises: 14 },
-    ],
-  };
+  const course = "Half stack application development";
+  const parts = [
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
+  ];
 
   return (
     <div>
-      <Header name={course.name} />
-      <Content parts={course} />
-      <Total parts={course} />
+      <Header name={course} />
+      {/*<Content parts={parts} />*/}
+      {/* <Total parts={course} /> */}
     </div>
   );
 };
