fffc5284565cc5cb305b242fd3235eaf739eb2d6
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2023-05-03 17:36:35 -0700

    Completed Statistics portion in unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,8 +8,8 @@ const Header = (props) => {
   );
 };
 
-const Button = ({ setFunction, text }) => {
-  return <button onClick={setFunction}>{text}</button>;
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
 };
 
 const Statistics = ({ category, value }) => {
@@ -26,26 +26,46 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [positive, setPositive] = useState(0);
 
-  const setValue = (ratingSet, ratingValue) => {
+  const incrementValue = (ratingSet, ratingValue, categoryNumber) => {
     const newValue = ratingValue + 1;
-    console.log("Value of is ", newValue);
+    const newTotal = total + 1;
+    console.log("Value is ", newValue);
     ratingSet(newValue);
+    setTotal(newTotal);
+    switch (categoryNumber) {
+      case 1:
+        setAverage((newValue - bad) / newTotal);
+        setPositive((newValue / newTotal) * 100);
+        break;
+      case 2:
+        setAverage((good - newValue) / newTotal);
+        setPositive((good / newTotal) * 100);
+        break;
+      default:
+        setPositive((good / newTotal) * 100);
+    }
   };
 
   return (
     <div>
       <Header text="Give feedback" />
-      <Button setFunction={() => setValue(setGood, good)} text={"Good"} />
+      <Button onClick={() => incrementValue(setGood, good, 1)} text={"Good"} />
       <Button
-        setFunction={() => setValue(setNeutral, neutral)}
+        onClick={() => incrementValue(setNeutral, neutral)}
         text={"Neutral"}
       />
-      <Button setFunction={() => setValue(setBad, bad)} text={"Bad"} />
+      <Button onClick={() => incrementValue(setBad, bad, 2)} text={"Bad"} />
       <Header text="Statistics" />
       <Statistics category={"Good"} value={good} />
       <Statistics category={"Neutral"} value={neutral} />
       <Statistics category={"Bad"} value={bad} />
+      <Statistics category={"All"} value={total} />
+      <Statistics category={"Average"} value={average} />
+      <Statistics category={"Positive"} value={positive} />
     </div>
   );
 };
