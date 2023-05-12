7a1cc7877207ffe50f64d408a32fbd669adfb01f
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2023-05-12 16:53:55 -0700

    Separated course into a separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,29 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>;
-
-const Total = ({ parts }) => {
-  let total = parts.reduce((current, next) => current + next.exercises, 0);
-  return <b>Total of {total} exercises</b>;
-};
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => {
-  return parts.map((part) => <Part part={part} />);
-};
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
