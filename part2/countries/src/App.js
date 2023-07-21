e5ba0603f0a0c7a9ce7abcdaa2c3058cd4007d9e
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2023-07-20 18:09:01 -0700

    Completed 2.18

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -9,6 +9,23 @@ const SearchBar = ({ handleChange }) => {
   );
 };
 
+const CountryInfo = ({ name, capital, area, languageList, flagUrl }) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>Capital: {capital}</p>
+      <p>Area: {area} km sq. </p>
+      <h3>Languages</h3>
+      <ul>
+        {languageList.map((language) => (
+          <li>{language}</li>
+        ))}
+      </ul>
+      <img src={flagUrl} />
+    </div>
+  );
+};
+
 const SearchResult = ({ listArray }) => {
   if (listArray.length > 10) {
     return <div>Too many matches, specify another filter.</div>;
@@ -19,6 +36,18 @@ const SearchResult = ({ listArray }) => {
           <p>{country.name.common}</p>
         ))}
       </div>
+    );
+  } else if (listArray.length == 1) {
+    const country = listArray[0];
+    const languages = country.languages;
+    return (
+      <CountryInfo
+        name={country.name.common}
+        capital={country.capital}
+        area={country.area}
+        languageList={Object.values(languages)}
+        flagUrl={country.flags.png}
+      />
     );
   }
 };
